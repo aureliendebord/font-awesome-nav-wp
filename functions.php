f95c2bde80333b61acc3232630846849326d2983
Aurélien Debord <contact@aureliendebord.com>
2015-07-15 10:30:47 +0200

    Update Path

--- functions.php
+++ functions.php
@@ -11,6 +11,6 @@ function enqueue_font_awesome() {
 
 //* Load locally Font Awesome externaly
 function enqueue_our_required_stylesheets(){
-    wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.css'); 
+    wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome/css/font-awesome.css'); 
 }
 add_action('wp_enqueue_scripts','enqueue_our_required_stylesheets');
